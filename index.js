c37fbe8ff79f5eebec152976a93a66097fe8fa7d
angeloxx <angeloxx@angeloxx.it>
2017-01-08 19:13:57 +0100

    Avoid messages overlap on the same object Adds dimmer control

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ class LegrandMyHome {
 		this.config.devices.forEach(function (accessory) {
 			this.log.info("LegrandMyHome: adds accessory");
 			accessory.parent = this;
-			this.devices.push(new MHRelay(this.log,accessory))
+
+			if (accessory.accessory == 'MHRelay') this.devices.push(new MHRelay(this.log,accessory))
+			if (accessory.accessory == 'MHDimmer') this.devices.push(new MHDimmer(this.log,accessory))
 		}.bind(this));
 	}
 
@@ -92,6 +94,62 @@ class MHRelay {
 				this.log.debug(sprintf("getPower %s = %s",this.address, this.power));
 				callback(null, this.power);
 			});
+		return [service, this.lightBulbService];
+	}
+}
+
+class MHDimmer {
+	constructor(log, config) {
+		this.mh = config.parent.controller;
+		this.name = config.name;
+		this.address = config.address;
+		this.displayName = config.name;
+		this.UUID = UUIDGen.generate(config.address);
+		this.log = log;
+		
+		this.power = false;
+		this.bri = 100;
+		this.sat = 0;
+		this.hue = 0;
+		this.log.info(sprintf("LegrandMyHome::MHRelay create object: %s", this.address));
+	}
+
+	getServices() {
+		var service = new Service.AccessoryInformation();
+		service.setCharacteristic(Characteristic.Name, this.name)
+			.setCharacteristic(Characteristic.Manufacturer, "Legrand MyHome")
+			.setCharacteristic(Characteristic.Model, "Dimmer")
+			.setCharacteristic(Characteristic.SerialNumber, "Address " + this.address);
+
+		this.lightBulbService = new Service.Lightbulb(this.name);
+
+		this.lightBulbService.getCharacteristic(Characteristic.On)
+			.on('set', (level, callback) => {
+				this.log.debug(sprintf("setPower %s = %s",this.address, level));
+				this.power = (level > 0);
+				if (this.power && this.bri == 0) {
+					this.bri = 100;
+				}
+				this.mh.relayCommand(this.address,this.power)
+				callback(null);
+			})
+			.on('get', (callback) => {
+				this.log.debug(sprintf("getPower %s = %s",this.address, this.power));
+				callback(null, this.power);
+			});
+
+		this.lightBulbService.getCharacteristic(Characteristic.Brightness)
+			.on('set', (level, callback) => {
+				this.log.debug(sprintf("setBrightness %s = %d",this.address, level));
+				this.bri = parseInt(level);
+				this.power = (this.bri > 0);
+				this.mh.dimmerCommand(this.address,this.bri)
+				callback(null);
+			})
+			.on('get', (callback) => {
+				this.log(sprintf("getBrightness %s = %d",this.address, this.bri));
+				callback(null, this.bri);
+			});			
 		return [service, this.lightBulbService];
 	}
 }
